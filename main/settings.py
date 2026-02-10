d685e9071637524a34c745025982729af8686c47
loopboot <chocoflakes16@gmail.com>
2026-02-10 12:33:11 +0800

    set allowed host

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = "django-insecure--p*gzj^hts08he5zrkuh)(wj@o@=p2n&ld%&36-$$pt7wfuut=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "0.0.0.0",
+    "e-learning-site-gjfk.onrender.comg",
+]
 
 
 # Application definition
